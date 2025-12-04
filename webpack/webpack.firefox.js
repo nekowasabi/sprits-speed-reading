7a152682ecc59115e98588452fe851cf24b2353f
nekowasabi <nolifeking00@gmail.com>
2025-12-04 22:29:19 +0900

    fix: Firefoxビルド設定でManifest V2を確実に使用
    - mergeWithCustomize で plugins: 'replace' を使用
    - DefinePlugin と HtmlWebpackPlugin を明示的に追加
    - Manifest V2 (background.scripts) を使用し service_worker 警告を解消

--- webpack/webpack.firefox.js
+++ webpack/webpack.firefox.js
@@ -1,9 +1,12 @@
 const { mergeWithCustomize, customizeArray } = require('webpack-merge');
 const path = require('path');
+const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const { version } = require('../package.json');
 
+// Use 'replace' strategy for plugins to completely override common config plugins
 module.exports = mergeWithCustomize({
   customizeArray: customizeArray({
     plugins: 'replace',
@@ -14,6 +17,11 @@ module.exports = mergeWithCustomize({
     path: path.resolve(__dirname, '../dist-firefox'),
   },
   plugins: [
+    // DefinePlugin (same as common)
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+    }),
+    // Firefox: Use manifest.v2.json with background.scripts (not service_worker)
     new CopyPlugin({
       patterns: [
         {
@@ -30,6 +38,12 @@ module.exports = mergeWithCustomize({
           to: 'icons',
         },
       ],
+    }),
+    // HtmlWebpackPlugin for options.html
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/options.html'),
+      filename: 'options.html',
+      chunks: ['options'],
     }),
   ],
 });
